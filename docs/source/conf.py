a85d2e0b23e200d254ea6ad73f20f7f618dcc4c6
Hailin Wang <hailin.wang@connect.polyu.hk>
2022-09-24 21:43:35 +0800

    The first commit

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'python-package'
+copyright = '2022, WANG Hailin'
+author = 'WANG Hailin'
+
+# The full version, including alpha/beta/rc tags
+_default_version = '0.0.1-unknown'
+try:
+    version = pkg_resources.get_distribution('python-package').version
+except pkg_resources.DistributionNotFound:
+    version = _default_version
+version = release
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'numpydoc',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.linkcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
+    'sphinx_codeautolink',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-package",
+    author="WANG Hailin",
+    author_email="hailin.wang@connect.polyu.hk",
+    description="Type hints for Abaqus/Python scripting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haiiliin/python-package",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        'Bug Tracker': 'https://github.com/haiiliin/python-package/issues',
+        'Documentation': 'https://python-package.haiiliin.com',
+        'Anaconda': 'https://anaconda.org/haiiliin/python-package',
+        'Read the Docs': 'https://readthedocs.org/projects/python-package',
+    },
+    use_scm_version={
+        "root": ".",
+        "relative_to": __file__,
+        "local_scheme": "node-and-date",
+        "write_to": "/_version.py",
+        "fallback_version": "0.0.1-unknown",
+    },
+    setup_requires=['setuptools_scm'],
+    python_requires='>=3.7',
+    package_dir={'': 'src'},
+    install_requires=['typing-extensions', 'setuptools_scm'],
+    packages=setuptools.find_packages('.'),
+)
